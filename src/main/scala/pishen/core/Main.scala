2726ed77c5572a2989a11741f6b9d4cb227b209c
pishen <pishen02@gmail.com>
2013-09-28 20:21:59 +0800

    debug

--- src/main/scala/pishen/core/Main.scala
+++ src/main/scala/pishen/core/Main.scala
@@ -10,17 +10,25 @@ object Main {
   def main(args: Array[String]): Unit = {
     val dbHandler = new DBHandler("new-graph-db")
 
-    val testCases = dbHandler.records.filter(r => {
+    val testCase = TestCase(dbHandler.records.take(1).toSeq.head, 0.1, 50)
+    logger.info("source: " + testCase.source.name)
+    logger.info("rank")
+    testCase.cocitationRank.foreach(logger info _._1.name)
+    logger.info("answer")
+    testCase.answers.foreach(logger info _.name)
+    logger.info("AP: " + testCase.cocitationAP)
+    
+    /*val testCases = dbHandler.records.filter(r => {
       logger.info("check record: " + r.name)
       r.outgoingRecords.length >= 50
     }).map(r => {
       logger.info("create testcase")
       TestCase(r, 0.1, 150)
-    }).toSeq
+    }).toSeq*/
     
     //logger.info("APs: " + testCases.map(_.cocitationAP).mkString(", "))
     //logger.info("length: " + testCases.length)
-    logger.info("MAP: " + (testCases.map(_.cocitationAP).sum / testCases.length))
+    //logger.info("MAP: " + (testCases.map(_.cocitationAP).sum / testCases.length))
     
   }
 }
