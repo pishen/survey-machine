d05849a42b83f3c78f2c6d0dc58d0c163a0b4a53
pishen <pishen02@gmail.com>
2013-10-12 18:26:10 +0800

    rerun new cocitation

--- src/main/scala/pishen/core/Main.scala
+++ src/main/scala/pishen/core/Main.scala
@@ -22,7 +22,7 @@ object Main {
       }
     }))*/
 
-    dbHandler.records.filter(r => {
+    /*dbHandler.records.filter(r => {
       logger.info("check record: " + r.name)
       r.citationType == Record.CitationType.Number
     }).foreach(r => {
@@ -37,7 +37,7 @@ object Main {
           tx.finish()
         }
       }
-    })
+    })*/
 
     /*dbHandler.records.find(r => ContentParser.detectType(r) == Record.CitationType.Number) match {
       case Some(r) => {
@@ -76,7 +76,7 @@ object Main {
     logger.info("NC MAP of 10: " + (testCases.map(_.newCocitationAP).sum / 10))
     logger.info("NC best AP of 10: " + (testCases.map(_.newCocitationAP).max))*/
 
-    /*val testCases = dbHandler.records.filter(r => {
+    val testCases = dbHandler.records.filter(r => {
       logger.info("check record: " + r.name)
       //r.citationType == Record.CitationType.Number &&
       r.outgoingRecords.filter(_.citationType == Record.CitationType.Number).length >= 12
@@ -93,7 +93,7 @@ object Main {
     //logger.info("K best of 10 MAP: " + (testCases.map(_.map(_.katzAP).max).sum / testCases.length))
     //logger.info("K avg of 10 MAP: " + (testCases.map(_.map(_.katzAP).sum / 10).sum / testCases.length))
     logger.info("NC best of 10 MAP: " + (testCases.map(_.map(_.newCocitationAP).max).sum / testCases.length))
-    logger.info("NC avg of 10 MAP: " + (testCases.map(_.map(_.newCocitationAP).sum / 10).sum / testCases.length))*/
+    logger.info("NC avg of 10 MAP: " + (testCases.map(_.map(_.newCocitationAP).sum / 10).sum / testCases.length))
 
     //citations
 
