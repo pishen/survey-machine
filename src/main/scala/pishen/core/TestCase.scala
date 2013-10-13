0bb1cab0e77fccf9e58cb676951f42b5c3754e0c
pishen <pishen02@gmail.com>
2013-10-13 17:40:55 +0800

    divide by 500

--- src/main/scala/pishen/core/TestCase.scala
+++ src/main/scala/pishen/core/TestCase.scala
@@ -69,7 +69,7 @@ object TestCase {
           }).map(targetRef => {
             val distance = ref.offsets.flatMap(offset =>
               targetRef.offsets.map(targetOffset => (targetOffset - offset).abs))
-              .min / 1000.0
+              .min / 500.0
             (targetRef.endRecord.get, if (distance < 1) 1 - distance else 0)
           })
         })
