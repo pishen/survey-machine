a6b0f9f038e9cdfd2e1615155851888e95226b63
pishen <pishen02@gmail.com>
2013-11-15 20:19:23 +0800

    count for "references"

--- src/main/scala/pishen/core/Main.scala
+++ src/main/scala/pishen/core/Main.scala
@@ -21,6 +21,14 @@ object Main {
     //update offsets
     //update longest pair distance?
     
-    dbHandler.records
+    val parsed = dbHandler.records.map(r => {
+      r.fileContent match {
+        case Some(c) => (true, c.lines.count(_.toLowerCase() == "references") > 0)
+        case None => (false, false)
+      }
+    })
+    
+    logger.info("withContent: " + parsed.count(_._1))
+    logger.info("with references: " + parsed.count(p => p._1 && p._2))
   }
 }
