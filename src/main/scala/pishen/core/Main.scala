a1f70da5f8241ada5838fd739961d8a1cb5aaf55
pishen <pishen02@gmail.com>
2014-01-18 16:30:13 +0800

    increase proxy

--- src/main/scala/pishen/core/Main.scala
+++ src/main/scala/pishen/core/Main.scala
@@ -5,8 +5,9 @@ import scala.annotation.elidable.ASSERTION
 import scala.sys.process.stringSeqToProcess
 import scala.sys.process.stringToProcess
 import scala.util.Random
-
 import org.slf4j.LoggerFactory
+import scalax.io.Resource
+import java.io.File
 
 object Main {
   private val logger = LoggerFactory.getLogger("Main")
@@ -14,6 +15,8 @@ object Main {
   def main(args: Array[String]): Unit = {
     "mkdir google-scholar".!
 
+    val ports = 10000 to 10010
+    var port = ports.head
     val papers = new PaperIterator
     papers.filter(p => {
       (p \ "year").text.toInt >= 2010 &&
@@ -22,20 +25,29 @@ object Main {
       val key = (p \ "@key").text.replaceAll("/", "-")
       val title = (p \ "title").text.replaceAll(" ", "+")
       logger.info("download scholar info: " + key)
-      val res = download("http://scholar.google.com.tw/scholar?q=" + title,
-        "google-scholar/" + key + ".html")
-      assert(res == 0)
-      Thread.sleep(2000 + Random.nextInt(3000))
+
+      val filename = "google-scholar/" + key + ".html"
+      val file = new File(filename)
+
+      if (!file.exists() || Resource.fromFile(file).string.contains("302 Moved")) {
+        download("http://scholar.google.com.tw/scholar?q=" + title, filename, port)
+
+        assert(!Resource.fromFile(file).string.contains("302 Moved"))
+        
+        port = Random.shuffle(ports.filter(_ != port)).head
+        Thread.sleep(5000)
+      }
     })
   }
 
-  def download(url: String, output: String) = {
+  def download(url: String, output: String, port: Int) = {
     //"http://scholar.google.com.tw/scholar?q=" + title
+    val ver = Random.shuffle(20 to 26).head.toString + ".0"
     Seq(
       "curl",
       "-o", output,
-      "-A", "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:26.0) Gecko/20100101 Firefox/26.0",
-      "--socks5", "localhost:9999",
+      "-A", "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:" + ver + ") Gecko/20100101 Firefox/" + ver,
+      "--socks5", "localhost:" + port,
       url).!
   }
 
