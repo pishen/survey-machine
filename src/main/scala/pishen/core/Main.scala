e5ab9c303f08734c27f0ff30ab40c7c253940193
pishen <pishen02@gmail.com>
2014-01-20 20:37:47 +0800

    download scholar, acm, pdf

--- src/main/scala/pishen/core/Main.scala
+++ src/main/scala/pishen/core/Main.scala
@@ -16,67 +16,99 @@ object Main {
   private val logger = LoggerFactory.getLogger("Main")
 
   def main(args: Array[String]): Unit = {
-    val scholar = new File("google-scholar/conf-afrigraph-Skala10.html")
-    val doc = Jsoup.parse(scholar, "UTF-8", "http://scholar.google.com.tw/")
-    val str = doc.select("div.gs_r").first().select("a").iterator()
-      .filter(_.attr("href").endsWith(".pdf"))
-      .map(_.attr("href"))
-      .mkString("\n")
-    /*val cleaner = new HtmlCleaner
-    val node = cleaner.clean(Resource.fromFile(scholar).string)
-    val serializer = new SimpleHtmlSerializer(cleaner.getProperties())
-    val str = serializer.getAsString(node)*/
-    /*val raw = Resource.fromFile(scholar).string
-    val start = raw.indexOf("<body>")
-    val end = raw.indexOf("</body>") + 7
-    val str = raw.substring(start, end).replaceAll("&*;", "")*/
-    println(str)
-    //val xml = XML.loadString(str)
-    /*val count = scholar.listFiles().count(scholar => {
-      val xml = XML.loadString(Resource.fromFile(scholar).string)
-      xml.\\("div").find(_.\("@class").text == "gs_r") match {
-        case Some(div) => div.\\("a").find(_.\("@href").text.endsWith(".pdf")).nonEmpty
-        case None => false
-      }
+    val scholarDir = new File("google-scholar")
+
+    val count = scholarDir.listFiles().count(scholar => {
+      println(scholar.getName())
+      val divs = Jsoup.parse(scholar, "UTF-8", "http://scholar.google.com.tw/").select("div.gs_r")
+      if (divs.isEmpty())
+        false
+      else
+        divs.first().select("a").iterator().find(_.attr("href").endsWith(".pdf")).nonEmpty
     })
-    println(count)*/
+    println(count)
   }
-  
-  def parseDBLP() = {
+
+  def parseDBLPAndDownload() = {
     "mkdir google-scholar".!
+    "mkdir dl-acm".!
+    "mkdir paper-pdf".!
 
     val ports = 10001 to 10010
     var port = ports.head
+
+    def downloadScholar(key: String, title: String) = {
+      val file = new File("google-scholar/" + key + ".html")
+      if (!file.exists() || Resource.fromFile(file).string.contains("302 Moved")) {
+        curl("http://scholar.google.com.tw/scholar?q=" + title, file.getPath(), port)
+        assert(!Resource.fromFile(file).string.contains("302 Moved"))
+        true
+      } else {
+        false
+      }
+    }
+
+    def downloadACM(key: String, ee: String) = {
+      val file = new File("dl-acm/" + key + ".html")
+      if (!file.exists()) {
+        val doid = ee.split("/").last
+        val url = "http://dl.acm.org/citation.cfm?doid=" + doid + "&preflayout=flat"
+        curl(url, file.getPath(), port)
+        true
+      } else {
+        false
+      }
+    }
+
+    def downloadPDF(key: String) = {
+      val pdfFile = new File("paper-pdf/" + key + ".pdf")
+      if (!pdfFile.exists()) {
+        val scholarFile = new File("google-scholar/" + key + ".html")
+        Jsoup.parse(scholarFile, "UTF-8", "http://scholar.google.com.tw/")
+          .select("div.gs_r")
+          .first()
+          .select("a").iterator()
+          .map(_.attr("href"))
+          .find(_.endsWith(".pdf")) match {
+            case Some(url) => {
+              curl(url, pdfFile.getPath(), port)
+              true
+            }
+            case None => false
+          }
+      } else {
+        false
+      }
+    }
+
     val papers = new PaperIterator
     papers.filter(p => {
       (p \ "year").text.toInt >= 2010 &&
         (p \ "ee").text.startsWith("http://doi.acm.org")
     }).foreach(p => {
       val key = (p \ "@key").text.replaceAll("/", "-")
+      logger.info("paper: " + key)
+      
       val title = (p \ "title").text.replaceAll(" ", "+")
-      logger.info("download scholar info: " + key)
+      val ee = (p \ "ee").text
 
-      val filename = "google-scholar/" + key + ".html"
-      val file = new File(filename)
-
-      if (!file.exists() || Resource.fromFile(file).string.contains("302 Moved")) {
-        download("http://scholar.google.com.tw/scholar?q=" + title, filename, port)
-
-        assert(!Resource.fromFile(file).string.contains("302 Moved"))
-        
+      val res1 = downloadScholar(key, title)
+      val res2 = downloadACM(key, ee)
+      val res3 = downloadPDF(key)
+      
+      if(res1 || res2 || res3){
         port += 1
-        if(port > ports.last) port = ports.head
-        
+        if (port > ports.last) port = ports.head
         Thread.sleep(10000)
       }
     })
   }
 
-  def download(url: String, output: String, port: Int) = {
-    //"http://scholar.google.com.tw/scholar?q=" + title
+  def curl(url: String, output: String, port: Int) = {
     val ver = Random.shuffle(20 to 26).head.toString + ".0"
     Seq(
       "curl",
+      "-L",
       "-o", output,
       "-A", "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:" + ver + ") Gecko/20100101 Firefox/" + ver,
       "--socks5", "localhost:" + port,
