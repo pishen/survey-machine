0fa7b9d9638250e59953404d9a39a9e13cb67971
pishen <pishen02@gmail.com>
2014-02-14 20:55:56 +0800

    change name

--- src/main/scala/db/Neo4j.scala
+++ src/main/scala/db/Neo4j.scala
@@ -13,7 +13,7 @@ import org.neo4j.graphdb.RelationshipType
 import org.neo4j.graphdb.Direction
 
 object Neo4j {
-  val graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("graph-db-old")
+  val graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("graph-db-text")
 
   sys.ShutdownHookThread {
     logger.info("shutdown graphDb")
