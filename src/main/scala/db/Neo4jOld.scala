6dc311ab88875f127a4684de51ca91e6266f0d6f
pishen <pishen02@gmail.com>
2014-02-14 21:15:44 +0800

    allow upgrade

--- src/main/scala/db/Neo4jOld.scala
+++ src/main/scala/db/Neo4jOld.scala
@@ -11,9 +11,13 @@ import org.neo4j.tooling.GlobalGraphOperations
 import org.neo4j.graphdb.Node
 import org.neo4j.graphdb.RelationshipType
 import org.neo4j.graphdb.Direction
+import org.neo4j.graphdb.factory.GraphDatabaseSettings
 
 object Neo4jOld {
-  val graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("graph-db-old")
+  val graphDb = new GraphDatabaseFactory()
+    .newEmbeddedDatabaseBuilder("graph-db-old")
+    .setConfig(GraphDatabaseSettings.allow_store_upgrade, "true")
+    .newGraphDatabase()
 
   sys.ShutdownHookThread {
     logger.info("shutdown old graphDb")
@@ -21,9 +25,9 @@ object Neo4jOld {
   }
 
   //Nodes
-  
+
   val recordIndex = graphDb.index().forNodes("RECORD_INDEX")
-  
+
   def getRecord(name: String) = withTx {
     val node = recordIndex.get("NAME", name).getSingle()
     if (node != null) Some(node.getId()) else None
@@ -37,21 +41,21 @@ object Neo4jOld {
     val res = graphDb.getNodeById(nodeId).getProperty(key, null).asInstanceOf[String]
     if (res == null) None else Some(res)
   }
-  
+
   //Relationships
-  
+
   def getRels(nodeId: Long, relType: RelationshipType, direction: Direction) = withTx {
     graphDb.getNodeById(nodeId).getRelationships(relType, direction).map(_.getId()).toSeq
   }
-  
+
   def getStartNode(relId: Long) = withTx {
     graphDb.getRelationshipById(relId).getStartNode().getId()
   }
-  
+
   def getEndNode(relId: Long) = withTx {
     graphDb.getRelationshipById(relId).getEndNode().getId()
   }
-  
+
   //Transaction
 
   def withTx[R](operations: => R) = {
